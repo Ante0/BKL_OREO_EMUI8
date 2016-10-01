99ba7e445ff10bf738753502fe8e1a8b47d230ae
Miklos Szeredi <mszeredi@redhat.com>
2016-10-01 07:32:32 +0200

    fuse: fix killing s[ug]id in setattr
    commit a09f99eddef44035ec764075a37bace8181bec38 upstream.
    
    Fuse allowed VFS to set mode in setattr in order to clear suid/sgid on
    chown and truncate, and (since writeback_cache) write.  The problem with
    this is that it'll potentially restore a stale mode.
    
    The poper fix would be to let the filesystems do the suid/sgid clearing on
    the relevant operations.  Possibly some are already doing it but there's no
    way we can detect this.
    
    So fix this by refreshing and recalculating the mode.  Do this only if
    ATTR_KILL_S[UG]ID is set to not destroy performance for writes.  This is
    still racy but the size of the window is reduced.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1743,7 +1743,6 @@ static int fuse_setattr(struct dentry *entry, struct iattr *attr)
 {
 	struct inode *inode = d_inode(entry);
 	struct file *file = (attr->ia_valid & ATTR_FILE) ? attr->ia_file : NULL;
-
 	int ret;
 
 	if (!fuse_allow_current_process(get_fuse_conn(inode)))
@@ -1777,7 +1776,6 @@ static int fuse_setattr(struct dentry *entry, struct iattr *attr)
 		return 0;
 
 	ret = fuse_do_setattr(inode, attr, file);
-
 	if (!ret) {
 		/* Directory mode changed, may need to revalidate access */
 		if (d_is_dir(entry) && (attr->ia_valid & ATTR_MODE))
