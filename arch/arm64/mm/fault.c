e368557f1c555a15c6f38858aade4078d996ca42
James Morse <james.morse@arm.com>
2016-10-18 11:27:46 +0100

    arm64: cpufeature: Schedule enable() calls instead of calling them via IPI
    commit 2a6dcb2b5f3e21592ca8dfa198dcce7bec09b020 upstream.
    
    The enable() call for a cpufeature/errata is called using on_each_cpu().
    This issues a cross-call IPI to get the work done. Implicitly, this
    stashes the running PSTATE in SPSR when the CPU receives the IPI, and
    restores it when we return. This means an enable() call can never modify
    PSTATE.
    
    To allow PAN to do this, change the on_each_cpu() call to use
    stop_machine(). This schedules the work on each CPU which allows
    us to modify PSTATE.
    
    This involves changing the protype of all the enable() functions.
    
    enable_cpu_capabilities() is called during boot and enables the feature
    on all online CPUs. This path now uses stop_machine(). CPU features for
    hotplug'd CPUs are enabled by verify_local_cpu_features() which only
    acts on the local CPU, and can already modify the running PSTATE as it
    is called from secondary_start_kernel().
    
    Reported-by: Tony Thompson <anthony.thompson@arm.com>
    Reported-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    [Removed enable() hunks for features/errata v4.4. doesn't have. Changed
     caps->enable arg in enable_cpu_capabilities()]
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -689,7 +689,6 @@ int cpu_enable_pan(void *__unused)
 	WARN_ON_ONCE(in_interrupt());
 
 	config_sctlr_el1(SCTLR_EL1_SPAN, 0);
-	asm(SET_PSTATE_PAN(1));
 	return 0;
 }
 #endif /* CONFIG_ARM64_PAN */
