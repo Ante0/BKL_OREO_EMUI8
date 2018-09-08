d4e6a804f16624697cf620b81ee92e1773cfca07
ante0 <ante0@ubuntu.ubuntu-domain>
2018-09-08 19:12:25 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@
 #export CROSS_COMPILE=aarch64-linux-android-
 #export PATH=$PATH:$AARCH64_COMPILR
 make ARCH=arm64 O=out/ merge_kirin970_defconfig
-make ARCH=arm64 O=out/ menuconfig
 make ARCH=arm64 O=out/ -j8
