aad1df03f5c5793f05c5414e794a2ac03fbf1d16
ante0 <ante0@ubuntu.ubuntu-domain>
2018-09-23 09:57:09 +0200

    ip6_outout.c removed )

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1365,7 +1365,7 @@ emsgsize:
 	     (skb && skb_is_gso(skb))) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&
 	    (rt->dst.dev->features & NETIF_F_UFO) &&
-	    (sk->sk_type == SOCK_DGRAM) && !udp_get_no_check6_tx(sk))) {
+	    (sk->sk_type == SOCK_DGRAM) && !udp_get_no_check6_tx(sk)) {
 		err = ip6_ufo_append_data(sk, queue, getfrag, from, length,
 					  hh_len, fragheaderlen, exthdrlen,
 					  transhdrlen, mtu, flags, fl6);
